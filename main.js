e5e666e3276ccdcf375f0220a0ad0a9b8ed2ccfb
Gray Smith <gray.smith00@gmail.com>
2018-03-11 20:01:30 +0000

    small change in js file

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ btn.addEventListener('click', function(){
     // make the request
     const XHR = new XMLHttpRequest();
     
+    // when request is completed run the function
     XHR.onreadystatechange = function(){
+        // if request state is complete and it worked
         if(XHR.readyState == 4 && XHR.status == 200) {
             const url = JSON.parse(XHR.responseText).message;
             img.src = url;
@@ -16,5 +18,4 @@ btn.addEventListener('click', function(){
     XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
     XHR.send();
 });
-
 
